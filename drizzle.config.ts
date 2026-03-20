f52ce34efb48e24bd25526ce56846091ce1cd239
RaviRajBhardwaj <exploringwith.ravirajbhar@gmail.com>
2026-03-21 05:19:39 +0530

    feat: setup db & schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'drizzle-kit';
+import fs from 'fs';
+import path from 'path';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+function getLocalD1DB() {
+	try {
+		const basePath = path.resolve('.wrangler');
+		const dbPath = path.resolve(basePath, 'state/v3/d1/miniflare-D1DatabaseObject');
+		const files = fs.readdirSync(dbPath);
+		for (const file of files) {
+			if (file.endsWith('.sqlite')) {
+				return path.resolve(dbPath, file);
+			}
+		}
+	} catch (err) {}
+	return '';
+}
+
+const localDbUrl = getLocalD1DB();
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-	dialect: 'postgresql',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	out: './drizzle',
+	dialect: 'sqlite',
+	// Automatically use local SQLite file for Drizzle Studio if it's found
+	...(localDbUrl ? { dbCredentials: { url: localDbUrl } } : { driver: 'd1-http' }),
 	verbose: true,
 	strict: true
 });
