3da7a1f8c48672d30e0ca8187786f68deb81fb13
RaviRajBhardwaj <exploringwith.ravirajbhar@gmail.com>
2026-03-21 23:27:07 +0530

    feat: add ui from shadcn-svelte

---
+++ src/lib/components/ui/carousel/carousel-previous.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { WithoutChildren } from 'bits-ui';
+	import { getEmblaContext } from './context.js';
+	import { cn } from '$lib/utils.js';
+	import { Button, type Props } from '$lib/components/ui/button/index.js';
+	import { HugeiconsIcon } from '@hugeicons/svelte';
+	import { ArrowLeft01Icon } from '@hugeicons/core-free-icons';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		variant = 'outline',
+		size = 'icon-sm',
+		...restProps
+	}: WithoutChildren<Props> = $props();
+
+	const emblaCtx = getEmblaContext('<Carousel.Previous/>');
+</script>
+
+<Button
+	data-slot="carousel-previous"
+	{variant}
+	{size}
+	aria-disabled={!emblaCtx.canScrollPrev}
+	disabled={!emblaCtx.canScrollPrev}
+	class={cn(
+		'absolute touch-manipulation rounded-full',
+		emblaCtx.orientation === 'horizontal'
+			? '-start-12 top-1/2 -translate-y-1/2'
+			: 'start-1/2 -top-12 -translate-x-1/2 rotate-90',
+		className
+	)}
+	onclick={emblaCtx.scrollPrev}
+	onkeydown={emblaCtx.handleKeyDown}
+	{...restProps}
+	bind:ref
+>
+	<HugeiconsIcon icon={ArrowLeft01Icon} strokeWidth={2} />
+	<span class="sr-only">Previous slide</span>
+</Button>
